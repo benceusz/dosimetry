aa5b14c109e7bda4386d7e953e134e21becb8e0b
nraresearch <bence.nemeth@usz.ch>
2022-04-21 11:50:47 +0200

    try cycle , skip patient if folder already exists. Try block. Warning suppression

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 % main script to calculate dfifferent doses for all the patients
-
+warning('off','all')
 listPercentages = [37, 39, 45, 55, 66, 100];
 pathPreprocessDicoms = "/media/nraresearch/Elements/Dual_Split_Projekt_ETH/preprocessed_dual_split"
 pathOutputGenerated = "/media/nraresearch/Elements/Dual_Split_Projekt_ETH/generated_dicoms_all"
@@ -11,28 +11,37 @@ end
 list_patients = GetSubDirsFirstLevelOnly(pathPreprocessDicoms);
 
 for i = 1:length(list_patients)
-    i_patient = list_patients(i)
-    
-    outputFolderPatient = fullfile(pathOutputGenerated, i_patient);
+    try
+        length(list_patients)
+        i_patient = list_patients(i)
+        outputFolderPatient = fullfile(pathOutputGenerated, i_patient);
 
-    if ~exist(outputFolderPatient, 'dir')
-       mkdir(outputFolderPatient)
+        if exist(outputFolderPatient, 'dir')
+           disp(["Patient dir already exist", i_patient])
+           continue
+        end
+        path_folder33 = fullfile(pathPreprocessDicoms, i_patient, "33");
+        list_33  = GetSubDirsFirstLevelOnly(path_folder33);
+        path33 = fullfile(path_folder33, list_33{1});
+
+        path_folder66 = fullfile(pathPreprocessDicoms, i_patient, "66");
+        list_66  = GetSubDirsFirstLevelOnly(path_folder66);
+        path66 = fullfile(path_folder66, list_66{1});
+
+        if ~exist(outputFolderPatient, 'dir')
+           mkdir(outputFolderPatient)
+        end
+
+        for p = 1:length(listPercentages)
+            i_percentage = listPercentages(p)
+            disp(["processing patient:", i_patient, " with dose percentage: ", num2str(i_percentage)])
+            i_percentage = listPercentages(p);
+            mix33_66(path33,path66, i_percentage, outputFolderPatient);
+
+        end
+    catch
+        disp(["ERROR: Crushed calculation at patient: ", i_patient])
     end
-
-    path_folder33 = fullfile(pathPreprocessDicoms, i_patient, "33");
-    list_33  = GetSubDirsFirstLevelOnly(path_folder33);
-    path33 = fullfile(path_folder33, list_33{1});
-
-    path_folder66 = fullfile(pathPreprocessDicoms, i_patient, "66");
-    list_66  = GetSubDirsFirstLevelOnly(path_folder66);
-    path66 = fullfile(path_folder66, list_66{1});
-
-    for p = 1:length(listPercentages)
-        disp(["processing patient:", i_patient, " with dose percentage: ", num2str(i_percentage)])
-        i_percentage = listPercentages(p);
-        mix33_66(path33,path66, i_percentage, outputFolderPatient);
-    
-    end       
 end
 
 
