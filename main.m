ab6f17bcaf06d508536b7c3e14dd4cc4ce79beb4
nraresearch <bence.nemeth@usz.ch>
2022-05-23 13:25:26 +0200

    for cycle runs further if try catches a failure at one image

--- main.m
+++ main.m
@@ -14,49 +14,57 @@ end
 list_patients = GetSubDirsFirstLevelOnly(pathPreprocessDicoms);
 
 for i = 1:length(list_patients)
-    try
-        length(list_patients)
-        i_patient = list_patients(i)
-        outputFolderPatient = fullfile(pathOutputGenerated, i_patient);
+    
+    length(list_patients)
+    i_patient = list_patients(i)
+    outputFolderPatient = fullfile(pathOutputGenerated, i_patient);
 
-        if exist(outputFolderPatient, 'dir')
-           disp(["Patient dir already exist", i_patient])
-           continue
-        end
-        path_folder33 = fullfile(pathPreprocessDicoms, i_patient, "33");
-        list_33  = GetSubDirsFirstLevelOnly(path_folder33);
-        path33 = fullfile(path_folder33, list_33{1});
+    if exist(outputFolderPatient, 'dir')
+       disp(["Patient dir already exist", i_patient])
+       continue
+    end
+    path_folder33 = fullfile(pathPreprocessDicoms, i_patient, "33");
+    list_33  = GetSubDirsFirstLevelOnly(path_folder33);
+    path33 = fullfile(path_folder33, list_33{1});
 
-        path_folder66 = fullfile(pathPreprocessDicoms, i_patient, "66");
-        list_66  = GetSubDirsFirstLevelOnly(path_folder66);
-        path66 = fullfile(path_folder66, list_66{1});
+    path_folder66 = fullfile(pathPreprocessDicoms, i_patient, "66");
+    list_66  = GetSubDirsFirstLevelOnly(path_folder66);
+    path66 = fullfile(path_folder66, list_66{1});
 
-        path_folder100 = fullfile(pathPreprocessDicoms, i_patient, "100");
-        list_100  = GetSubDirsFirstLevelOnly(path_folder100);
-        path100 = fullfile(path_folder100, list_100{1});
-        
-        if ~exist(outputFolderPatient, 'dir')
-           mkdir(outputFolderPatient)
-        end
+    path_folder100 = fullfile(pathPreprocessDicoms, i_patient, "100");
+    list_100  = GetSubDirsFirstLevelOnly(path_folder100);
+    path100 = fullfile(path_folder100, list_100{1});
 
-        for p = 1:length(listPercentages)
+    if ~exist(outputFolderPatient, 'dir')
+       mkdir(outputFolderPatient)
+    end
+
+    for p = 1:length(listPercentages)
+        try
             i_percentage = listPercentages(p)
             disp(["processing patient:", i_patient, " with dose percentage: ", num2str(i_percentage)])
             i_percentage = listPercentages(p);
             mix_da_db(path33, 50, path66, 100, i_percentage, outputFolderPatient);
+        catch
+            disp(["ERROR: Crushed calculation at patient: ", outputFolderPatient])
         end
-        
-        % mix 33 and 100 to get 66
-        disp(["processing patient:", i_patient, " with dose percentage: ", num2str(66)])
-        mix_da_db(path33, 50, path100, 150, 66, outputFolderPatient);
-
-        % mix 66 and 100 to get 100
-        mix_da_db(path66, 100, path100, 150, 150 , outputFolderPatient);
-        
-        
-    catch
-        disp(["ERROR: Crushed calculation at patient: ", i_patient])
     end
+
+    % mix 33 and 100 to get 66
+    disp(["processing patient:", i_patient, " with dose percentage: ", num2str(66)])
+    try
+        mix_da_db(path33, 50, path100, 150, 66, outputFolderPatient);
+    catch
+        disp(["ERROR: Crushed calculation at patient: ", outputFolderPatient])
+    end
+    
+    % mix 66 and 100 to get 100
+    try
+        mix_da_db(path66, 100, path100, 150, 150 , outputFolderPatient);
+    catch
+        disp(["ERROR: Crushed calculation at patient: ", outputFolderPatient])
+    end
+        
 end
 
 
